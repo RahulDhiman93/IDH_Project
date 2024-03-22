0c80f7c1b5f06f807dacb436cd53e94b5ec7c602
Rahul Dhiman <rsdm34@gmail.com>
2024-03-21 20:39:42 -0700

    Patient Model added, Parsing function to parse instructions.txt implemented

--- idh_thp.playground/Contents.swift
+++ idh_thp.playground/Contents.swift
@@ -1,3 +1,34 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+//MARK: Patient Model
+struct Patient {
+    let id: Int
+    let name: String
+    var exams: Set<Int> = []
+}
+
+//MARK: PatientRecords class for CRUD operations
+class PatientRecords {
+    
+    //MARK: Patients dictionary to record patient data
+    private var patients: [Int: Patient] = [:]
+    
+    //MARK: Parsing instructions.txt file to retrieve operations information
+    func processInstructions() {
+        guard let filePath = Bundle.main.path(forResource: "instructions", ofType: "txt") else {
+            print("No such file found!")
+            return
+        }
+        do {
+            let contents = try String(contentsOfFile: filePath)
+            let lines = contents.components(separatedBy: .newlines)
+            print(lines)
+        } catch {
+            print("Error reading file: \(error)")
+        }
+    }
+}
+
+//MARK: Using PatientRecords Class and it's functions
+let records = PatientRecords()
+records.processInstructions()
